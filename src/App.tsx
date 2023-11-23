b2151c23365e5dc0da7dd12791c665bbeb1ab6a9
selelel <136282944+selelel@users.noreply.github.com>
2023-11-23 15:30:50 +0800

    Still on development

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <>
       <div className="bg-green_ fixed top-0 right-0 left-0 z-10 p-1 border-b border-green-900 ">
-        {/* <p className="text-center text-sm text-white ">
+        <p className="text-center text-sm text-white ">
           Website is on Development
-        </p> */}
+        </p>
         <Nav />
       </div>
       <Routes>
